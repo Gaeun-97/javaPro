6a6d6d97ffed652d6cffcfb3c01761405e66f8ca
Gaeun <riha0201@naver.com>
2025-02-17 17:10:57 +0900

    test4

--- javaPro/src/days11/EX03.java
+++ javaPro/src/days11/EX03.java
@@ -5,18 +5,17 @@ package days11;
 
 /**
  * @author user
- *	@date-2025. 2. 17.오전 8:58:38
+ *	@date-2025. 2. 17.오후 5:09:40
  * @subject
  * @content
  */
-public class Ex01 {
+public class EX03 {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("hello");
-		// >는 수정되었다는 뜻
+		// TODO Auto-generated method stub
 
 	}
 
