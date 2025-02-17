5e330ad000b5633bedaba3ed04b9eb7dc2be0eee
Gaeun <riha0201@naver.com>
2025-02-17 16:35:31 +0900

    test2

---
+++ javaPro/src/days07_1/Ex01_02.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package days07_1;
+
+/**
+ * @author user
+ *	@date-2025. 2. 11.오전 10:41:39
+ * @subject
+ * @content
+ */
+public class Ex01_02 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.printf("%d(%c)\n",(int)'가','가');
+		System.out.printf("%d(%c)\n",(int)'힣','힣');
+
+	}
+
+}
